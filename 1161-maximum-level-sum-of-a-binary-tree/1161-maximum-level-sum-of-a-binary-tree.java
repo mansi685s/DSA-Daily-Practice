1f1e83d84bc8697515f0a3cc02c8f6df1337af8b
Mansi sahu <mansisahu75150@gmail.com>
2026-07-24 01:22:53 +0530

    Time: 9 ms (62.98%), Space: 49.3 MB (41.32%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -23,17 +23,14 @@ class Solution {
         int k=1;
         while(!q.isEmpty()){
             int size=q.size();
-            ArrayList<Integer>l1=new ArrayList<>();
+           int sum=0;
             while(size-->0){
                 TreeNode curr=q.poll();
-                l1.add(curr.val);
+                sum+=(curr.val);
                 if(curr.left!=null)q.offer(curr.left);
                 if(curr.right!=null)q.offer(curr.right);
             }
-            int sum=0;
-            for(int i=0;l1.size()>i;i++){
-                sum+=l1.get(i);
-            }
+           
             if(sum>maxsum){
                 maxsum=sum;
                 level=k;
